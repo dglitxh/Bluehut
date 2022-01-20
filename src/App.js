66b7518e604d76fc7b52f2f87995f87cb9edcf22
YD <ydglitch@gmail.com>
2022-01-20 04:21:46 +0000

    complete setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import About from "./components/About"
 
 function App() {
   return (
     <div className="App">
-      born stunna
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
